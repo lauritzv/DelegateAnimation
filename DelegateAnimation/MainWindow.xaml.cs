417a175d37874712d1b426fc7ad4f8f3c2f90701
lauritzv <lauritzv@gmail.com>
2018-03-04 22:56:54 +0100

    Add project files.

---
+++ DelegateAnimation/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace DelegateAnimation
+{
+    public partial class MainWindow : Window
+    {
+        public delegate void TimeDelegate(int time);
+        public event TimeDelegate moveIt;
+        private System.Windows.Threading.DispatcherTimer timer;
+        private int time = 0;
+        Random random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            populateSpace(500);
+            startTimer(60000);
+        }
+
+        private void startTimer(int tickInterval)
+        {
+            timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Interval = new TimeSpan(tickInterval);
+            timer.Tick += (sender, eventargs) => moveIt(time++); //event on tick
+            timer.Start();
+        }
+
+        private void populateSpace(int amount)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                Planet p = new Planet(
+                    random.Next(2048),                  //timeoffset
+                    random.Next(1, 4),                  //orbitspeed
+                    random.Next(50, 150),               //orbitradius
+                    newEllipse(random.Next(20, 40)));   //shape + add to canvas
+
+                moveIt += p.CalculatPos;
+            }
+        }
+
+        private Ellipse newEllipse(int diameter)
+        {
+            Ellipse el = new Ellipse();
+            el.Height = diameter;
+            el.Width = diameter;
+            el.Fill = new SolidColorBrush(Color.FromRgb( (byte)random.Next(255), (byte)random.Next(255), (byte)random.Next(255) ));
+            
+            spaceCanvas.Children.Add(el);
+            return el;
+        }
+
+    } //windowclass
+}//ns
---
+++ DelegateAnimation/Planet.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace DelegateAnimation
+{
+    class Planet
+    {
+        public int timeOffset       { get; private set; }
+        public int orbitSpeed       { get; private set; }
+        public int orbitalRadius    { get; private set; }
+        private Canvas c;
+        private Ellipse ell;
+        public Ellipse shape
+        {
+            get { return ell; }
+            set { c = (Canvas)value.Parent; ell = value; }
+        }
+        
+        public Planet(int timeOffset, int orbitSpeed, int orbitalRadius, Ellipse shape)
+        {
+            this.timeOffset = timeOffset;
+            this.orbitSpeed = orbitSpeed;
+            this.orbitalRadius = orbitalRadius;
+            this.shape = shape;
+        }
+
+        public void CalculatPos(int time)
+        {
+            if (c != null)
+            {
+                time += timeOffset;
+                
+                int posx = (int)(Math.Cos(time * orbitSpeed * Math.PI / 180) * orbitalRadius);
+                int posy = (int)(Math.Sin(time * orbitSpeed * Math.PI / 180) * orbitalRadius);
+                
+                Canvas.SetLeft(shape, posx + c.Width / 2 - shape.Width / 2);
+                Canvas.SetTop(shape, posy + c.Height / 2 - shape.Width / 2);
+            }
+        }
+
+    } //class
+} //ns
